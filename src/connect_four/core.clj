732049965756718f30b1d18203f6fbd14917d46d
magoeke <magoeke@htwg-konstanz.de>
2016-07-23 14:30:07 +0200

    Refactored core.clj. Renamed most functions in a hopefully more clojure way.

--- src/connect_four/core.clj
+++ src/connect_four/core.clj
@@ -29,28 +29,28 @@
   []
   (def turn-count (inc turn-count)))
 
-(defn setCell
+(defn set-cell
   "Sets cell in field."
   [player y x]
   (if (= "-" (get-in field [y x]))
     (update-field (assoc-in field [y x] player))
     (turn-count-down)))
 
-(defn currentPlayer
+(defn current-player
   "Returns sign of current player."
   []
   (if (= 0 (mod turn-count 2)) "X" "O"))
 
-(defn prepareCommandToSet
+(defn prepare-command-to-set
   "Prepares command to call setCell."
   [command]
   (turn-count-up)
   (let [coords (str/split command #",")]
-    (setCell (currentPlayer)
+    (set-cell (current-player)
       (Integer. (get coords 0))
       (Integer. (get coords 1)))))
 
-(defn showCommands
+(defn show-commands
   "Outputs all possible commands."
   []
   (println "Commands: stop; Y,X; new"))
@@ -68,28 +68,30 @@
   (function argument)
   (draw-field))
 
-(defn evaluateCommand
+(defn evaluate-command
   "Evaluates command from user input."
   [command]
   (cond
     (= command "new")(wrap-draw println "not implemented yet")
-    (re-matches #"[0-9]+,[0-9]+" command)(wrap-draw prepareCommandToSet command)
+    (re-matches #"[0-9]+,[0-9]+" command)(wrap-draw prepare-command-to-set command)
     :else (println "command doesn't exist")))
 
-(defn handleInput
+(declare game-loop) ; feels dirty
+
+(defn handle-input
   "Handles Input from user. input != stop"
   [input]
-  (evaluateCommand (str input))
-  (gameloop))
+  (evaluate-command (str input))
+  (game-loop))
 
-(defn gameloop
+(defn game-loop
   "This function represents the game loop."
   []
-  (showCommands)
+  (show-commands)
   (let [input (read-line)]
     (println input)
     (if (not= input "stop")
-      (handleInput input)
+      (handle-input input)
       (println "I hope you enjoyed the game."))))
 
-(gameloop)
+; (game-loop)
