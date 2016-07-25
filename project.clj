7e97ae88e17e2bc5150adb6d8ea8c822d68bfe74
magoeke <magoeke@htwg-konstanz.de>
2016-07-25 17:42:19 +0200

    Added description.

--- project.clj
+++ project.clj
@@ -1,6 +1,5 @@
-(defproject connect-four "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject connect-four "0.1.0"
+  :description "Small Five Wins in Clojure. Game has only a tui."
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
